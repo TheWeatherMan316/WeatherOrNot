74c82d89398b578b2307480d6e7645a4e7e2f5da
Michael Kranzdorf <michael.kranzdorf@xitaso.com>
2020-07-14 16:59:43 +0200

    implemented temp timer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Interface from "./components/Interface";
 
 import "./App.css";
 
 function App() {
+  console.log("Initi");
   const [temp, setTemp] = useState("--");
   const [bar, setBar] = useState("--");
 
@@ -20,13 +21,18 @@ function App() {
     return (Math.random() * (max - min) + min).toFixed(precision).toString();
   }
 
+  useEffect(() => {
+    setInterval(() => {
+      tempCalc();
+    }, 6000);
+  }, []);
+
   return (
     <div className="app" align="center">
       <div className="container">
         <Header />
         <Interface display={temp} measure={tempCalc} unit="°C" label="Temperature" />
         <Interface display={bar} measure={barCalc} unit="mbar" label="Barometric Pressure" />
-        {console.log(temp)}
       </div>
     </div>
   );
--- src/components/Interface.js
+++ src/components/Interface.js
@@ -6,6 +6,7 @@ import Label from "./Interface/Label";
 export default function Interface(props) {
   return (
     <div className="Interface">
+      {console.log(" got rerendered")}
       <Label label={props.label} />
       <Display display={props.display} unit={props.unit} />
       <Controls measure={props.measure} />
