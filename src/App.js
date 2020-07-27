808971896f45659fcb9570b5970bb2271b5962ae
Michael Kranzdorf <michael.kranzdorf@xitaso.com>
2020-07-27 13:58:56 +0200

    not really a lot of progress

--- src/App.js
+++ src/App.js
@@ -54,10 +54,8 @@ function App() {
 
   const tempHistComponent = () => {
     return (
-      <div className="container" id="tempHist">
-        <div>
-          <ul>{tempHistory}</ul>
-        </div>
+      <div>
+        <ul>{tempHistory}</ul>
       </div>
     );
   };
@@ -107,10 +105,8 @@ function App() {
 
   const barHistComponent = () => {
     return (
-      <div className="container" id="barHist">
-        <div>
-          <ul>{barHistory}</ul>
-        </div>
+      <div>
+        <ul>{barHistory}</ul>
       </div>
     );
   };
@@ -124,47 +120,45 @@ function App() {
 
   return (
     <div className="app" align="center">
-      <Router>
-        <Switch>
+      <div className="container">
+        <Router>
           <Route path="/">
-            <div className="container">
-              <Header />
-              <DisplayRow label="Time" display={<CurrentDate time={true} />} />
-              <DisplayRow label="Date" display={<CurrentDate time={false} />} />
-              <DisplayRow
-                label="Temperature"
-                display={<UnitValue value={temp.toFixed(1).toString()} unit="°C" />}
-                action={<Controls action={measureTemp} buttonLabel="measure" />}
-                history={
-                  <Link to="/tempHist">
-                    <Controls buttonLabel="history" />
-                  </Link>
-                }
-              />
-              <DisplayRow
-                label="Average Temperature"
-                display={<UnitValue value={averageTemp.toFixed(1).toString()} unit="°C Ø" />}
-              />
-              <DisplayRow
-                label="Barometric Pressure"
-                display={<UnitValue value={bar.toFixed(0).toString()} unit="mbar" />}
-                action={<Controls action={measureBar} buttonLabel="measure" />}
-                history={
-                  <Link to="/barHist">
-                    <Controls buttonLabel="history" />
-                  </Link>
-                }
-              />
-              <DisplayRow
-                label="Barometric Pressure Trend"
-                display={<TrendValue measurements={barHistArr} />}
-              />
-            </div>
+            <Header />
+            <DisplayRow label="Time" display={<CurrentDate time={true} />} />
+            <DisplayRow label="Date" display={<CurrentDate time={false} />} />
+            <DisplayRow
+              label="Temperature"
+              display={<UnitValue value={temp.toFixed(1).toString()} unit="°C" />}
+              action={<Controls action={measureTemp} buttonLabel="measure" />}
+              history={
+                <Link to="/tempHist">
+                  <Controls buttonLabel="history" />
+                </Link>
+              }
+            />
+            <DisplayRow
+              label="Average Temperature"
+              display={<UnitValue value={averageTemp.toFixed(1).toString()} unit="°C Ø" />}
+            />
+            <DisplayRow
+              label="Barometric Pressure"
+              display={<UnitValue value={bar.toFixed(0).toString()} unit="mbar" />}
+              action={<Controls action={measureBar} buttonLabel="measure" />}
+              history={
+                <Link to="/barHist">
+                  <Controls buttonLabel="history" />
+                </Link>
+              }
+            />
+            <DisplayRow
+              label="Barometric Pressure Trend"
+              display={<TrendValue measurements={barHistArr} />}
+            />
           </Route>
           <Route path="/tempHist">{tempHistComponent()}</Route>
           <Route path="/barHist">{barHistComponent()}</Route>
-        </Switch>
-      </Router>
+        </Router>
+      </div>
     </div>
   );
 }
