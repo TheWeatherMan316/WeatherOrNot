16222881ca57e02cbb82694549a69d7f1ccb5773
Michael Kranzdorf <michael.kranzdorf@xitaso.com>
2020-07-29 12:35:17 +0200

    solve styling issues according to requirement

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Fragment } from "react";
 import Header from "./components/Header";
 import DisplayRow from "./components/DisplayRow";
 import TrendValue from "./components/DisplayRow/BoxContent/TrendValue";
@@ -46,16 +46,29 @@ function App() {
   }
 
   const tempHistory = tempHistoryArr.map((element) => (
-    <li>
-      {element.time.toLocaleDateString("de-DE")} | {element.time.toLocaleTimeString("de-DE")} |
-      {element.value.toFixed(1).toString()}°C
-    </li>
+        <Fragment>
+        <tr>
+          <td>{element.time.toLocaleDateString("de-DE")}</td>
+          <td>{element.time.toLocaleTimeString("de-DE")}</td>
+          <td>{element.value.toFixed(1).toString()}°C</td>
+        </tr>
+      </Fragment>
   ));
 
   const tempHistComponent = () => {
     return (
-      <div>
-        <ul>{tempHistory}</ul>
+      <div className="history">
+        <button className="button">
+          <Link to="/home">Back</Link>
+        </button>
+        <table>
+          <tr>
+            <th>Date</th>
+            <th>Time</th>
+            <th>Measurement</th>
+          </tr>
+          {tempHistory}
+        </table>
       </div>
     );
   };
@@ -97,16 +110,29 @@ function App() {
   }
 
   const barHistory = barHistArr.map((element) => (
-    <li>
-      {element.time.toLocaleDateString("de-DE")} | {element.time.toLocaleTimeString("de-DE")} |
-      {element.value.toFixed(0).toString()} mBar
-    </li>
+    <Fragment>
+      <tr>
+        <td>{element.time.toLocaleDateString("de-DE")}</td>
+        <td>{element.time.toLocaleTimeString("de-DE")}</td>
+        <td>{element.value.toFixed(0).toString()} mBar</td>
+      </tr>
+    </Fragment>
   ));
 
   const barHistComponent = () => {
     return (
-      <div>
-        <ul>{barHistory}</ul>
+      <div className="history">
+        <button className="button">
+          <Link to="/home">Back</Link>
+        </button>
+        <table>
+          <tr>
+            <th>Date</th>
+            <th>Time</th>
+            <th>Measurement</th>
+          </tr>
+          {barHistory}
+        </table>
       </div>
     );
   };
@@ -118,45 +144,53 @@ function App() {
     // eslint-disable-next-line
   }, []);
 
+  const home = () => {
+    return (
+      <Fragment>
+        <Header />
+        <DisplayRow label="Time" display={<CurrentDate time={true} />} />
+        <DisplayRow label="Date" display={<CurrentDate time={false} />} />
+        <DisplayRow
+          label="Temperature"
+          display={<UnitValue value={temp.toFixed(1).toString()} unit="°C" />}
+          action={<Controls action={measureTemp} buttonLabel="measure" />}
+          history={
+            <button className="button">
+              <Link to="/tempHist">History</Link>
+            </button>
+          }
+        />
+        <DisplayRow
+          label="Average Temperature"
+          display={<UnitValue value={averageTemp.toFixed(1).toString()} unit="°C Ø" />}
+        />
+        <DisplayRow
+          label="Barometric Pressure"
+          display={<UnitValue value={bar.toFixed(0).toString()} unit="mbar" />}
+          action={<Controls action={measureBar} buttonLabel="measure" />}
+          history={
+            <button className="button">
+              <Link to="/barHist">History</Link>
+            </button>
+          }
+        />
+        <DisplayRow
+          label="Barometric Pressure Trend"
+          display={<TrendValue measurements={barHistArr} />}
+        />
+      </Fragment>
+    );
+  };
+
   return (
     <div className="app" align="center">
       <div className="container">
         <Router>
-          <Route path="/">
-            <Header />
-            <DisplayRow label="Time" display={<CurrentDate time={true} />} />
-            <DisplayRow label="Date" display={<CurrentDate time={false} />} />
-            <DisplayRow
-              label="Temperature"
-              display={<UnitValue value={temp.toFixed(1).toString()} unit="°C" />}
-              action={<Controls action={measureTemp} buttonLabel="measure" />}
-              history={
-                <Link to="/tempHist">
-                  <Controls buttonLabel="history" />
-                </Link>
-              }
-            />
-            <DisplayRow
-              label="Average Temperature"
-              display={<UnitValue value={averageTemp.toFixed(1).toString()} unit="°C Ø" />}
-            />
-            <DisplayRow
-              label="Barometric Pressure"
-              display={<UnitValue value={bar.toFixed(0).toString()} unit="mbar" />}
-              action={<Controls action={measureBar} buttonLabel="measure" />}
-              history={
-                <Link to="/barHist">
-                  <Controls buttonLabel="history" />
-                </Link>
-              }
-            />
-            <DisplayRow
-              label="Barometric Pressure Trend"
-              display={<TrendValue measurements={barHistArr} />}
-            />
-          </Route>
-          <Route path="/tempHist">{tempHistComponent()}</Route>
-          <Route path="/barHist">{barHistComponent()}</Route>
+          <Switch>
+            <Route path="/tempHist">{tempHistComponent()}</Route>
+            <Route path="/barHist">{barHistComponent()}</Route>
+            <Route path="/home">{home()}</Route>
+          </Switch>
         </Router>
       </div>
     </div>
--- src/components/DisplayRow/Controls.js
+++ src/components/DisplayRow/Controls.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 export default function Controls(props) {
+
   return (
-    <div className="controls">
-      <button type="button" class="button" onClick={props.action}>
-        {props.buttonLabel}
-      </button>
+    <div className="controls"><button type="button" class="button" onClick={props.action}>
+    {props.buttonLabel}
+  </button>
     </div>
   );
 }
